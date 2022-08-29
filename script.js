e5696984f7b5acaa68e1456c8ae21f0f309b3d31
grada92 <danielegradassai@gmail.com>
2022-08-29 18:48:49 +0200

    imposto carosello e pulsante avanti

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 console.log('js ok')
 
-const images = ['01', '02' , '03', '04', '05']
-let imageindex = 0;
+const images = ['01', '02' , '03', '04', '05'];
 
 
-const container = document.querySelector('carousel');
+const container = document.querySelector('.container-carousel');
 
-for (i = 0 ; i < images.lenght ; i++) {
-
+// Ciclo per immagini 
+for (i = 0 ; i < images.length ; i++) {
     const image = images[i];
-    const imageClass = i === imageindex ? 'object active' : 'object'
+    container.innerHTML += '<img class="object" src="img/'+ image +'.jpg" alt="due" />'
 }
+// Imposto la prima immagine visibile
+const firstImage = document.querySelector('.object');
+firstImage.classList.add('active');
 
+
+const previousButton = document.getElementById('previous');
+
+const nextButton = document.getElementById('next');
+
+// Imposto button next per scorrere immagini in avanti
+nextButton.addEventListener('click',
+    function(){
+        console.log('mi hai cliccato');
+
+        const activeImage = document.querySelector('.object.active');
+        console.log(activeImage);
+        const nextElement = activeImage.nextElementSibling; 
+        console.log(nextElement);
+
+        if (nextElement){
+            activeImage.classList.remove('active');
+            nextElement.classList.add('active');
+        }
+    }
+
+);
+
+// Imposto button previous per scorrere immagini indietro
+
+  
