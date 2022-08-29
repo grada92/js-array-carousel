5cca96041f5c254c7b9cca6edf1ba71caad5969f
grada92 <danielegradassai@gmail.com>
2022-08-29 15:25:32 +0200

    imposto javascript

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const images = ['01', '02' , '03', '04', '05']
 let imageindex = 0;
 
 
+const container = document.querySelector('carousel');
+
+for (i = 0 ; i < images.lenght ; i++) {
+
+    const image = images[i];
+    const imageClass = i === imageindex ? 'object active' : 'object'
+}
+
